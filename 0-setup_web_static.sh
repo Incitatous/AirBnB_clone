98c216c1ec86877115978aaf18f5165d5422b1d3
Incitatous <dcianberlin@gmail.com>
2017-03-15 14:29:04 -0700

    adding autoindex

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 # Prepares web servers for deployment
 sudo apt-get update
-sudo apt-get install -y nginx
-sudo apt-get install -y curl
+sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 echo "Holberton" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i "89i location /hbnb_static {\n\talias\t/data/web_static/current/;\n}" /etc/nginx/sites-enabled/default
+sudo sed -i "37i \n\n\tlocation /hbnb_static {\n\talias\t/data/web_static/current/;\n\tautoindex off;\n}" /etc/nginx/sites-enabled/default
 sudo service nginx restart
