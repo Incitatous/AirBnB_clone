e7e2cedad47d7ba5861cb8b7f83e320e23023814
Incitatous <dcianberlin@gmail.com>
2017-03-15 14:24:26 -0700

    flags order

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,7 +6,7 @@ sudo apt-get install -y curl
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 echo "Holberton" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -fs /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i "89i location /hbnb_static {\n\talias\t/data/web_static/current/;\n}" /etc/nginx/sites-enabled/default
 sudo service nginx restart
