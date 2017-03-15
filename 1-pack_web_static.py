df9a2b449aa067c8eac0a4c9ca4e14d70b880a4c
Incitatous <dcianberlin@gmail.com>
2017-03-15 16:46:18 -0700

    name fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -6,4 +6,4 @@ from fabric.api import local
 def do_pack():
     fileName = datetime.now().strftime("%Y%m%d%H%M%S")
     local('mkdir -p versions')
-    local('tar -cvzf "versions/%s.tgz" ./web_static' % fileName)
+    local('tar -cvzf "versions/web_static_%s.tgz" ./web_static' % fileName)
