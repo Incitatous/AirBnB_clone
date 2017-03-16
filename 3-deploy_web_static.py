9a757024c73b3e09a11c18350482c4dbff277910
Incitatous <dcianberlin@gmail.com>
2017-03-16 12:43:09 -0700

    task3

---
+++ 3-deploy_web_static.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+from datetime import datetime
+from fabric.api import run, put, sudo, local, env
+from os.path import isfile
+
+
+env.hosts = ['54.164.203.121', '54.152.248.231']
+env.user = 'ubuntu'
+
+
+def do_pack():
+    """
+    Sets up a server
+    """
+    fileName = datetime.now().strftime("%Y%m%d%H%M%S")
+    local('mkdir -p versions')
+    local('tar -cvzf "versions/web_static_%s.tgz" ./web_static' % fileName)
+
+
+def do_deploy(archive_path):
+    """
+    Distributes an archive to your web servers
+    """
+    if not isfile(archive_path):
+        print(archive_path + "Not a file")
+        return False
+    try:
+        cleanArchive = archive_path.split("/")[-1]
+        noExt = cleanArchive.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        sudo("mkdir -p {}{}".format(path, noExt))
+        sudo("tar -xzf /tmp/{} -C {}{}".format(cleanArchive, path, noExt))
+        sudo("rm /tmp/{}".format(cleanArchive))
+        sudo("mv {}{}/web_static/* {}{}".format(path, noExt, path, noExt))
+        sudo("rm -rf {}{}/web_static".format(path, noExt))
+        sudo("rm /data/web_static/current")
+        sudo("ln -s {}{} /data/web_static/current".format(path, noExt))
+        return True
+    except Exception:
+        return False
